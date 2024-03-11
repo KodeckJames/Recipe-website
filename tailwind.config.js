73b62724356978fa685a8946f7b34263bc15c8a2
Kodeck James <kodeckjames095@gmail.com>
2024-03-12 00:19:29 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200:'888883',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
